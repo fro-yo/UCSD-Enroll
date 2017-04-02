b74c82c42a332fe7382595461039a8c4d8c2567b
Ajeya Rengarajan <ajrengar@ucsd.edu>
2017-04-02 02:05:31 -0700

    initial structure + firebase

--- init_populate.js
+++ init_populate.js
@@ -1,4 +1,13 @@
 var socsjs = require('socsjs');
+var admin = require('firebase-admin');
+var serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://ucsd-enroll.firebaseio.com"
+});
+
+var db = admin.database();
 
 var quarter = 'FA16';
 var dept = 'CSE';
@@ -12,7 +21,6 @@ socsjs.searchDepartment(quarter, dept, timeout, undergrad).then(function(result)
 
             // if course not encountered
             if (deptObj [course.code] === undefined) {
-                console.log ('creating' + course.code);
                 deptObj[course.code] = {};
                 deptObj[course.code]['name'] = course.name;
                 deptObj [course.code]['lectures'] = [];
@@ -21,13 +29,15 @@ socsjs.searchDepartment(quarter, dept, timeout, undergrad).then(function(result)
             for (sectionNum in course.sections) {
                 var section = course.sections[sectionNum];
                 if (section.type === 'lecture') {
+                    section.openSeats = 'init',
+                    section.waitlistSize = 'init'
                     deptObj [course.code]['lectures'].push (section);
                 }
             }
         }
     }
 
-    console.log (deptObj);
+    console.log (JSON.stringify(deptObj));
 }).catch(function(err) {
     console.log(err, 'oops!');
 });
